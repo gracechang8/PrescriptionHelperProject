c4c82446c2ce547ab117edd5ae31773d8fab7ca8
gracechang8 <changgrace83@gmail.com>
2024-05-23 16:00:48 -0400

    Add files via upload

---
+++ Qt_6_7_0_for_iOS_Simulator/qtcsettings.cmake
@@ -0,0 +1,12 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Xcode" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/Users/gracechang/Library/Mobile Documents/com~apple~CloudDocs/SUMMER CODING/prehelp/build/Qt_6_7_0_for_iOS_Simulator/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/Users/gracechang/Qt/6.7.0/ios/bin/qmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/Users/gracechang/Qt/6.7.0/ios" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_TOOLCHAIN_FILE" "/Users/gracechang/Qt/6.7.0/ios/lib/cmake/Qt6/qt.toolchain.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_OSX_ARCHITECTURES" "x86_64" CACHE "STRING" "" FORCE)
+set("CMAKE_OSX_SYSROOT" "iphonesimulator" CACHE "STRING" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
